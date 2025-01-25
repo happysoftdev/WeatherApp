574825193726c8800b336a2b1870407b2553a1a0
Patricia Gh <pgh.softwareengineer@gmail.com>
2025-01-25 18:05:39 +0200

    Added list view with default locations & current location

--- WeatherApp/WeatherApp/Features/Cities/List/ListView.swift
+++ WeatherApp/WeatherApp/Features/Cities/List/ListView.swift
@@ -6,12 +6,39 @@
 //
 
 import SwiftUI
+import CoreLocation
 
 struct ListView: View {
     
-    @StateObject private var viewModel = CitiesListViewModel()
+    @StateObject private var weatherViewModel = CityForecastViewModel()
+    @StateObject private var locationManager = LocationManager()
+    
+    var defaultLocations: [String] = ["Tokyo", "London", "Barcelona"]
     
     var body: some View {
-        DetailsView()
+        NavigationStack {
+            List {
+                if let locationName = locationManager.locationName {
+                    NavigationLink {
+                        DetailsView()
+                    } label: {
+                        Text("Your location: \(locationName)")
+                    }
+                }
+                ForEach(defaultLocations, id: \.self) { location in
+                    NavigationLink {
+                        DetailsView()
+                    } label: {
+                        Text(location)
+                    }
+                }
+            }
+            .listStyle(.plain)
+            .onAppear {
+                locationManager.requestLocation()
+            }
+            .navigationTitle("Weather")
+            .padding()
+        }
     }
 }
